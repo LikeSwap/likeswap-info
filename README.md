81ca0767253fffe6b54441751a1737bd479ac227
LikeSwap <likeswaporg@gmail.com>
2022-01-10 10:11:17 +0700

    upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Likeswap History
+
+Check it out live: [https://info.likeswap.org/](https://info.likeswap.org/).
+
+### To Start Development
+
+###### Installing dependencies
+
+```bash
+yarn
+```
+
+###### Running locally
+
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/likeswap/likeswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://likeswap.org" rel="noopener noreferrer" target="_blank">
+      Likeswap
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
